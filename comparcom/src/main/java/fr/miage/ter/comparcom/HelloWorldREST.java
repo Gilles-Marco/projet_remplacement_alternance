13ac31f1c10835fefddfec38d5c55722a907d4f6
Marco Gilles <gilles.marco06@gmail.com>
2021-07-02 12:49:40 +0200

    page web configurateur de requête

--- comparcom/src/main/java/fr/miage/ter/comparcom/HelloWorldREST.java
+++ comparcom/src/main/java/fr/miage/ter/comparcom/HelloWorldREST.java
@@ -1,9 +1,11 @@
 package fr.miage.ter.comparcom;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class HelloWorldREST {
 
     @GetMapping("/helloworld")
